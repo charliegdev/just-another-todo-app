64e0e7059bb540a522802edc12ef666605ca6b15
Charlie Guan <charliegdev@gmail.com>
2018-09-07 00:20:49 -0700

    Add actions and action creators.

---
+++ src/States/actions.js
@@ -0,0 +1,18 @@
+// String constants, mainly to eliminate typos across the codebase
+const ADD_TODO = "ADD_TODO";
+const TOGGLE_TODO = "TOGGLE_TODO";
+const SET_VISIBILITY_FILTER = "SET_VISIBILITY_FILTER";
+
+const visibilityFilters = {
+  SHOW_ALL: "SHOW_ALL",
+  SHOW_COMPLETED: "SHOW_COMPLETED",
+  SHOW_ACTIVE: "SHOW_ACTIVE"
+};
+
+// Action creators
+const addTodo = text => ({ type: ADD_TODO, text });
+const toggleTodo = index => ({ type: TOGGLE_TODO, index });
+const setVisibilityFilter = filter => ({ type: SET_VISIBILITY_FILTER, filter });
+
+// Exporting everything; however, different files might only need a subset of these.
+export { ADD_TODO, TOGGLE_TODO, SET_VISIBILITY_FILTER, visibilityFilters, addTodo, toggleTodo, setVisibilityFilter };
---
+++ src/States/reducers.js
@@ -0,0 +1,2 @@
+import { combineReducers } from "redux";
+import { ADD_TODO, TOGGLE_TODO, SET_VISIBILITY_FILTER, visibilityFilters } from "./Actions";
