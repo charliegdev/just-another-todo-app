977432cf9f356667225aefe913f36a5599db0d8a
Charlie Guan <cguan@blastworksinc.com>
2018-09-07 18:40:41 -0700

    Integrate React with Semantic UI React.

--- src/App.js
+++ src/App.js
@@ -1,37 +1,19 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-
-import { addTodo, toggleTodo, setVisibilityFilter, visibilityFilters } from "./States/actions";
-import store from "./States/store";
-
-console.log(store.getState());
-const unsubscribe = store.subscribe(() => console.log(store.getState()));
-
-store.dispatch(addTodo("Item 1"));
-store.dispatch(addTodo("Item 2"));
-store.dispatch(addTodo("Item 3"));
-
-store.dispatch(toggleTodo(store.getState().todos[1].uuid)); // Kinda awkward to use uuid...
-// In an actual React app, UUID should be obtained as a prop, so it'll be less awkward.
-
-store.dispatch(setVisibilityFilter(visibilityFilters.SHOW_ACTIVE));
-
-unsubscribe();
+import { Container, Header, Icon } from "semantic-ui-react";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Container text>
+        <br />
+        <Header as="h1">
+          <Icon name="tasks" />
+          <Header.Content>
+            Just Another Todo App
+            <Header.Subheader>In React, Redux & Semantic UI React</Header.Subheader>
+          </Header.Content>
+        </Header>
+      </Container>
     );
   }
 }
